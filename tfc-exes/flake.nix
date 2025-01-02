f72d93a10e0b27d4ee7f899471673f452ec21592
Jón Bjarni <jbbjarnason@gmail.com>
2025-01-02 17:32:22 +0000

    Debug error in receiving frames from ethercat (#10)
    * testing in progress, debugging failed to receive
    
    * wait loop delay keeps the network running a bit longer
    
    * test bleeding edge ethercrab
    
    * Set wait loop delay to 1 ms and max_frames to 32
    
    * revert commenting out pdo logic
    
    * revert default feature  declaration
    
    * remove unwanted line
    
    * use newly released ethercrab instead of branch commit

---
+++ tfc-exes/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Run with nix develop";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            musl
+            (rust-bin.stable.latest.default.override {
+              targets = [ "x86_64-unknown-linux-musl" ];
+            })
+          ];
+        };
+      }
+    );
+}
